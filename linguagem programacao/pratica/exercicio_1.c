0d1eaf7dc315a6c3ad71dac64ab43aaf191f561a
Leonardo Felipe Salgado <53799801+Salgado2004@users.noreply.github.com>
2023-08-03 11:18:55 -0300

    Create exercicio_1.c

---
+++ linguagem programacao/pratica/exercicio_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//E1) Faça um programa que apresente os números de 1 a 10, sendo que cada número deve ser impresso em uma linha e os números ímpares deslocados (\t) a direita.
+
+int main(void) {
+    for (int x=1; x <=10; x++){
+        if (x%2 == 0){
+            printf("%d\n", x);
+        } else{
+            printf("\t%d\n", x);
+        }
+    }
+}
