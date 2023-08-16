6767f7d526ccd67b4e6bcb7b7472ee8e1494276f
Salgado2004 <leosalgado2004@gmail.com>
2023-08-16 11:15:21 -0300

    atividade 07 lista L1

---
+++ linguagem programacao/atividades/lista_L1_07.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+    int numeros[3];
+    int aux, key, tot=3;
+    
+    scanf("%d", &numeros[0]);
+    scanf("%d", &numeros[1]);
+    scanf("%d", &numeros[2]);
+    
+    do {
+        key=0;
+        tot--;
+        for (int a=0; a<tot; a++){
+            if (numeros[a]>numeros[a+1]){
+                aux = numeros[a];
+                numeros[a] = numeros[a+1];
+                numeros[a+1] = aux;
+                key=1;
+            }
+        }
+    } while(key==1);
+    
+    printf("%d\n", numeros[0]);
+    printf("%d\n", numeros[1]);
+    printf("%d\n", numeros[2]);
+}
