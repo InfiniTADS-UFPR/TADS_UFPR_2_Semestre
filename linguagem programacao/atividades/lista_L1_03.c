0711011f5d58c341913b2ed6f7006d0883e6d69f
Salgado2004 <leosalgado2004@gmail.com>
2023-08-16 14:03:19 -0300

    atividade 03 lista L1

---
+++ linguagem programacao/atividades/lista_L1_03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//E1_03) Faça um programa que apresente os números de pares de 0 a 20, cada número em uma linha.
+
+int main(){
+    for (int x=0; x<=20; x++){
+        printf("%d\n", x);
+        x++;
+    }
+}
