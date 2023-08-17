47b2f3c50367ad4479d0fffe6ec7a0a61bfb5fa3
Leonardo Felipe Salgado <53799801+Salgado2004@users.noreply.github.com>
2023-08-17 13:24:16 -0300

    Create lista_L1_02.c

---
+++ linguagem programacao/atividades/lista_L1_02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//E1_02) Faça um programa que apresente os números de 1 a 20, cada número em uma linha.
+
+int main(){
+    for (int x =1; x<=20; x++){
+        printf("%d\n", x);
+    }
+}
