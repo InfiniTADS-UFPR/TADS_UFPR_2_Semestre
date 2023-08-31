aea92251d0ef8b04251fb96b49ab64a5dadd5a1a
Salgado2004 <leosalgado2004@gmail.com>
2023-08-31 10:08:00 -0300

    exercise 1

---
+++ desenvolvimento web I/js-assignment/js/ex1.js
@@ -0,0 +1,12 @@
+console.log("// Exercício 1\n");
+
+/* Escreva um loop que realiza 7 chamadas do comando `console.log` e exibe o seguinte
+"triângulo" */
+
+let saida = '';
+for (let i = 0; i < 7; i++) {
+    saida += "#";
+    console.log(saida);
+}
+
+console.log("\n");
