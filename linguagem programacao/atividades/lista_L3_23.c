6a2ded9815f552e9adec9b1a98ed0fb80fed2176
Salgado2004 <leosalgado2004@gmail.com>
2023-09-06 23:08:27 -0300

    atividade 23 lista L3 corrigido

--- linguagem programacao/atividades/lista_L3_23.c
+++ linguagem programacao/atividades/lista_L3_23.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//Dados um vetor com a capacidade de 33 elementos, sendo que os 32 primeiros elementos representam dígitos binários (0,1) e o 33 o terminador da string. O conteúdo deverá ser lido como uma String (gets).
+//23) Dados um vetor com a capacidade de 33 elementos, sendo que os 32 primeiros elementos representam dígitos binários (0,1) e o 33 o terminador da string. O conteúdo deverá ser lido como uma String (gets).
 //Apresente-os formatando-os de forma que sejam apresentados blocos de 4 dígitos e um espaço entre os 8 blocos.
 
 void show(char str[]){
@@ -15,6 +15,6 @@ void show(char str[]){
 int main(void){
     char bin[33];
 
-    gets(bin);
+    scanf("%s", bin);
     show(bin);
 }
