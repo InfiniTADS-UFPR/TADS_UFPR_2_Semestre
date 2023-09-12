d40ae40f1e27acb1d0906a11c381b4b1c3861d6b
Salgado2004 <leosalgado2004@gmail.com>
2023-09-12 10:41:15 -0300

    atividade 27 lista L3 corrigido

--- linguagem programacao/atividades/lista_L3_27.c
+++ linguagem programacao/atividades/lista_L3_27.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
+#include <string.h>
 
 //E27) Faça um programa que leia uma String e apresente todos os sufixos da string em ordem léxica.
 
-void sufixos(char str[]){
-    //TO-DO: ordenar os sufixos
-    for (int i = 0; str[i] != '\0'; i++){
-        for (int j = i; str[j] != '\0'; j++){
-            printf("%c", str[j]);
+void sufixos(char str[], int size){
+    int i, j, aux, key, tot;
+    char suffix[size][size+1];
+    int ordem[size];
+    
+    for (i = 0; str[i] != '\0'; i++){
+        for (j = i; str[j] != '\0'; j++){
+            suffix[i][j-i] = str[j];
         }
-        if (str[i+1] != '\0'){
-            printf("\n");
-        }
+        suffix[i][j-i] = '\0';
     }
+
+    for (int i = 0; i < size; i++){
+        ordem[i] = i;
+    }
+
+    tot=size;
+    do {
+        key=0;
+        tot--;
+        for (int a=0; a<tot; a++){
+            if (strcmp(suffix[ordem[a]], suffix[ordem[a+1]])>0){
+                aux = ordem[a];
+                ordem[a] = ordem[a+1];
+                ordem[a+1] = aux;
+                key=1;
+            }
+        }
+    } while(key==1); 
+
+    for (i = 0; i < size; i++){
+        printf("%s\n", suffix[ordem[i]]);
+    }
+
 }
 
 int main(void){
     char str[100];
 
     scanf("%s", str);
-    sufixos(str);
+    sufixos(str, strlen(str));
 }
