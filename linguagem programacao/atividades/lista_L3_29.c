41aa1964ea1d3ae0a90023c33ec35c8afced64ff
Salgado2004 <leosalgado2004@gmail.com>
2023-09-12 10:49:19 -0300

    inicio atividade 29 lista L3

---
+++ linguagem programacao/atividades/lista_L3_29.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//E29) Faça um programa que leia duas Strings, verifique se elas formam anagramas, ignorando variação de caixa e espaços em brancos e apresente: "Formam anagramas" ou "Nao formam anagramas"
+
+int main(void){
+    char str1[100], str2[100];
+
+    printf("Digite a primeira string: ");
+    scanf("%s", str1);
+    printf("Digite a segunda string: ");
+    scanf("%s", str2);
+
+    
+
+}
